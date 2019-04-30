1b7d8526588537c03d0509605f63146394fc34f3
Marco Maggi <marco.maggi-ipsu@poste.it>
2019-04-30 17:29:58 +0200

    meta files cleanup

--- tmp/acinclude.m4
+++
@@ -1,5 +0,0 @@
-dnl dnl acinclude.m4
-
-m4_include(meta/autoconf/vicare.m4)
-
-dnl end of file
--- tmp/autogen.sh
+++
@@ -1,9 +0,0 @@
-# autogen.sh --
-#
-# Run this in the top source directory to rebuild the infrastructure.
-
-set -xe
-test -d meta/autotools			|| mkdir meta/autotools
-autoreconf --warnings=all --install --verbose "$@"
-
-### end of file
